394a8b5822a2c6c2510679d0ece36ea6304dab2d
Anuj C More <moreanuj1307@gmail.com>
2026-05-05 12:32:25 -0700

    Time: 163 ms (12.82%), Space: 37.1 MB (70%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,4 +1,18 @@
+"""
+Time complexity: O(n)
+Space complexity: O(1)
+"""
+
+
 class Solution:
+
+    def remove_left(self, nums, hashmap, left, summation):
+        summation -= nums[left]
+        hashmap[nums[left]] -= 1
+        if hashmap[nums[left]] == 0:
+            del hashmap[nums[left]]
+        return left + 1, summation
+
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         left = 0
         result = 0
@@ -11,17 +25,9 @@ class Solution:
             hashmap[element] = hashmap.get(element, 0) + 1
 
             while hashmap[element] > 1:
-                summation -= nums[left]
-                hashmap[nums[left]] -= 1
-                if hashmap[nums[left]] == 0:
-                    del hashmap[nums[left]]
-                left += 1
+                left, summation = self.remove_left(nums, hashmap, left, summation)
 
             if right - left + 1 == k:
                 result = max(result, summation)
-                summation -= nums[left]
-                hashmap[nums[left]] -= 1
-                if hashmap[nums[left]] == 0:
-                    del hashmap[nums[left]]
-                left += 1
+                left, summation = self.remove_left(nums, hashmap, left, summation)
         return result
