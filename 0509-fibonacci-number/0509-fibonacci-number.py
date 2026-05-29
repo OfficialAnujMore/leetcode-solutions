936efa6c3ea26a50715c9f0668955a896f0eec33
Anuj C More <moreanuj1307@gmail.com>
2026-05-29 12:12:42 -0700

    Time: 35 ms (97.36%), Space: 19.2 MB (57.4%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+
+        prev, curr = 0, 1
+
+        for i in range(2, n + 1):
+            summation = prev + curr
+            prev = curr
+            curr = summation
+
+        return curr
