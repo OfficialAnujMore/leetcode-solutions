ebabb6b5b6fe68fcc65cbfa35e5e095e1b0cb942
Anuj C More <moreanuj1307@gmail.com>
2026-07-07 11:06:51 -0700

    Time: 0 ms (100%), Space: 20.5 MB (18.73%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,7 +1,3 @@
-'''
-Time complexity - O(n^2) as the heights of the same subtrees are computed repeatedly
-'''
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,33 +6,20 @@ Time complexity - O(n^2) as the heights of the same subtrees are computed repeat
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-
         def height(root):
             if not root:
                 return 0
 
             left_height = height(root.left)
-            right_height = height(root.right)
+            if left_height == -1:
+                return -1
 
-            # print(
-            #     f"Node {root.val}: left height = {left_height}, "
-            #     f"right height = {right_height}"
-            # )
+            right_height = height(root.right)
+            if right_height == -1:
+                return -1
+            if abs(left_height - right_height) > 1:
+                return -1
 
             return 1 + max(left_height, right_height)
 
-        if not root:
-            return True
-        left_height = height(root.left)
-        right_height = height(root.right)
-
-        # print(
-        #     f"Checking node {root.val}: "
-        #     f"left height = {left_height}, right height = {right_height}"
-        # )
-
-        return (
-            abs(left_height - right_height) <= 1
-            and self.isBalanced(root.left)
-            and self.isBalanced(root.right)
-        )
+        return height(root) != -1
